597a3a1dc10d6150132b438185fe6a38ac933d80
Mehul Kaushik <97294636+Epicguest97@users.noreply.github.com>
2025-08-10 00:36:04 +0530

    Add solution - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth=0;
+        int maxi=0;
+        for(char c : s){
+            if(c=='(') depth++;
+            else if(c==')') depth--;
+            else continue;
+            if(depth>maxi)maxi=depth;
+        }
+        return maxi;
+    }
+};
